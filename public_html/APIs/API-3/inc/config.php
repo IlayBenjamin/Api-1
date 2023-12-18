6de05f386a79601996a79445b44aaaa54a247b30
Ilay Benjamin <ilaybenjamin1@gmail.com>
2023-12-18 18:40:50 +0200

    API-3 new

---
+++ public_html/APIs/API-3/inc/config.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "u398523801_api_admin1");
+define("DB_PASSWORD", "ilayBenjamin_2003");
+define("DB_DATABASE_NAME", "u398523801_api_db1");
